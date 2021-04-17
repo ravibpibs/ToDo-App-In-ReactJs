c919b7396fcfc763bd204c6bf0a90907b5fb7bb3
ravi ranjan jha <rjha4743@gmail.com>
2021-04-17 17:19:12 +0530

    First commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            My Name is Ravi Ranjan Jha
+            <p>Looking to obtain the position of Software Developer in a fast-paced tech environment to utilize well honed skills in computer science and extensive experience in developing customized software based on varying client demands.
+            </p>
+        </div>
+    )
+}
